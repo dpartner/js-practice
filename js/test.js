c13d9bef52793426e565e188656904566266d26e
dpartner <d.partner.ua@gmail.com>
2022-03-22 10:33:03 +0200

    просто тренировался

---
+++ js/test.js
@@ -0,0 +1,11 @@
+const article = document.querySelector('.article');
+console.log(article.innerHTML);
+
+// const title = document.querySelector('.article .title');
+// console.log(title.innerHTML);
+
+// const text = document.querySelector('.article .text');
+// console.log(text.innerHTML);
+
+// const link = document.querySelector('.article .link');
+// console.log(link.innerHTML);
